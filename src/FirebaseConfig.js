b0d11a319c08be49be7bda8b98263a38cee80605
Camilo <77551795+candresme@users.noreply.github.com>
2023-12-03 15:59:00 -0500

    montaje nuevo

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -10,19 +10,12 @@ import "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
   apiKey: "AIzaSyAvmNOEx2eKGw_Xm-3dkPYopWQ8n1-JL3g",
-
   authDomain: "sena-proyecto.firebaseapp.com",
-
   projectId: "sena-proyecto",
-
   storageBucket: "sena-proyecto.appspot.com",
-
   messagingSenderId: "257501147079",
-
   appId: "1:257501147079:web:38117e500e83a99ef5b3e2"
-
 };
 
 
@@ -31,3 +24,4 @@ export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
 const analytics = getAnalytics(app);
+
